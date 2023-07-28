0f73a2426c4d91a500d38e7b40daa0797444c1e6
HemanthSD07 <140800539+HemanthSD07@users.noreply.github.com>
2023-07-28 14:45:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,73 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
-
-
+const APIURL = 'https://api.github.com/users/'
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 const main = document.getElementById('main');
-
-
-getMovies(API_URL)
-async function getMovies(url){
-    const res = await fetch(url)
-    const data = await res.json()
-    showMovies(data.results)
-    console.log(data.results)
+async function getUser(username){
+try{
+const {data} = await axios(APIURL + username)
+createUserCard(data);
+getRepos(username)
+}catch(err){
+if(err.response.status == 404){
+createErrorCard('No profile with this username')
 }
-function showMovies(movies){
-    main.innerHTML=''
-    movies.forEach((movie)=>{
-        const{ title, poster_path,vote_average,overview}=movie
-        const movieEl=document.createElement('div')
-        movieEl.classList.add('movie')
-
-        movieEl.innerHTML= `
-        <img src="${IMG_PATH + poster_path}" alt="${title}">
-        <div class="movie-info">
-        <h3> MOVIE TITLE </h3>
-        <span 
-        class="${getClassByRate(vote_average)}">${vote_average}</span>
-        </div>
-        <div class="overview">
-        <h3>Overview</h3>
-        ${overview}
-        </div>
-        `
-        main.appendChild(movieEl)
-
-    })
-} 
-function getClassByRate(vote){
-    if(vote>=8){
-        return 'green'
-    }else if(vote>=5){
-        return 'orange'
-    }else{
-        return 'red'
-    }
 }
-form.addEventListener('submit', (e) =>{
-    e.preventDefault()
-const searchTerm = search.value
-if(searchTerm && searchTerm !==''){
-    getMovies(SEARCH_API + searchTerm)
-    search.value=''
-}else{
-    window.location.reload()
 }
-
+async function getRepos(username){
+try{
+console.log(username)
+const {data} = await axios(APIURL + username + '/repos?sort=created')
+addReposToCard(data);
+}catch(err){
+createErrorCard('Problem fetching repos')
+}
+}
+function createUserCard(user){
+const cardHTML = `<div class="card">
+<div>
+<img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+</div>
+<div class="user-info">
+<h2>${user.name}</h2>
+<p>${user.bio}</p>
+<ul>
+<li>${user.followers} <strong>Followers</strong></li>
+<li>${user.following} <strong>Following</strong></li>
+<li>${user.public_repos} <strong>Repos</strong></li>
+</ul>
+<div id="repos">
+</div>
+</div>
+</div>`
+main.innerHTML = cardHTML;
+}
+function createErrorCard(msg){
+const cardHTML = `
+<div class="card">
+<h1>${msg}</h1>
+</div>
+`
+main.innerHTML = cardHTML;
+}
+function addReposToCard(repos){
+const reposEl = document.getElementById('repos');
+console.log(reposEl)
+repos
+.slice(0,10)
+.forEach(repo => {
+const repoEl = document.createElement('a')
+repoEl.classList.add('repo')
+repoEl.href = repo.html_url
+repoEl.target = '_blank'
+repoEl.innerText = repo.name
+reposEl.appendChild(repoEl);
 })
-
+}
+form.addEventListener('submit', (e) => {
+e.preventDefault()
+const user = search.value
+if(user){
+getUser(user)
+search.value = ''
+}
+})
